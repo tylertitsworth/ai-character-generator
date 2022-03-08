a542217903010f990ea1782a0b1d26e9d2f40ff1
Tyler Titsworth <titswort@oregonstate.edu>
2022-03-07 18:27:20 -0800

    Titswort/vercel (#10)
    * emotion style template + apply template to downloads page
    
    * Restructuring previous pages; including Side-Navbar; very basic page start complete.
    
    * Quick commit before I have to step away. Formatting/configuring the getting-started page; working out the logic (NOT COMPLETE).
    
    * Working out similar logic in Backstory page; tiny bit of error handling. Still not really where I want it but a good place to check in at.
    
    * Edit to hook; Dropdowns being served.
    
    * merge into style branch
    
    * Fleshing out the pages more; have ability score displays. Going to be figuring out the skill dropdowns next.
    
    * Not my favorite solution, but serves different number of skill dropdowns (min 1 max 4) for whatever class it gets (right now is testClass). Will need to reconfigure for class saved in store.
    
    * Styling is there; BUT I CANNOT GET THESE PROFICIENCIES TO SHOW MORE THAN ONE OF THEMSELVES AND I AM BIG MAD ABOUT IT. But it looks good?
    
    * Quick add.
    
    * Reconfigured the Ability Scores to be dropdowns. Have them get a bit mad when two are the same, but not near to the degree that Tyler wants. Will have to add a validity state to the store to get the value back to the main page, in order to stop the user from progressing. But it does show what is missing from the scores, and does alert that a score can't have two values.
    
    * Opps, I committed with some wack borders. Fixed!
    
    * Finished with changes to background page.
    
    * Fixed Monk skill issue; now will check first (0th) array to see if the first choice has the word 'Skill: ' and now it flips appropriately.
    
    * Light @media working
    
    * add method to generate pdf + remove downloads page
    
    * merge index.js and generalcharacter.js and fix vercel errors
    
    Co-authored-by: Kira Jiroux <kira.jiroux@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,175 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React, { useState, useEffect } from 'react';
+import { ApolloProvider as ApolloProvider2 } from '@apollo/react-components'; // I hate this, why do I have to do this to get around invariant 29?
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
-
 import Link from 'next/link';
+import { useQuery } from '@apollo/client';
+import { query } from '../hooks/qltree';
+import Layout from '../components/Layout'
+import { useDispatch } from 'react-redux';
+import {
+	writeClass,
+	writeSubclass,
+	writeRace,
+	writeSubrace,
+	writeEquipment,
+	writeBackground,
+	writeSkills
+} from '../redux/actions'
+import { X } from '../node_modules/@emotion-icons/octicons/index';
+import { Action, FormDisplay, FlexColumn, FlexRow, ButtonDisplay } from '../styles/globals';
+import Dropdown from '../components/Dropdown';
+import SkillDropdown from '../components/SkillDropdown';
+import AbilityScoreDisplay from '../components/AbilityScoreDisplay';
 
-import { Provider } from 'react-redux';
-import store from '../redux/store';
+function TestStore(data) {
 
-import Head from 'next/head'
-import Image from 'next/image'
+	// testing values
+	var charClass = "Barbarian"
+	var subclass = "Berserker"
+	var race = "Tiefling"
+	var subrace = "Bloodline of Mammon"
+	var background = "Acolyte"
+	var equipment = ["Club", "Padded Armor"]
+	var skills = ["Athletics", "Nature"]
 
-import Layout from '../components/Layout';
+	console.log("Inside", data.classes)
+	const obj = Object.values(data.skills)
+	console.log("Object: ", obj[0].name)
 
-import GeneralCharacter from './generalcharacter'
+	const dispatch = useDispatch()
+	dispatch(writeRace(race))
+	dispatch(writeSubrace(subrace))
+	dispatch(writeClass(charClass))
+	dispatch(writeSubclass(subclass))
+	dispatch(writeBackground(background))
+	dispatch(writeEquipment(equipment))
+	dispatch(writeSkills(skills))
 
-const client = new ApolloClient({
-    uri: "https://www.dnd5eapi.co/graphql", 
-    cache: new InMemoryCache()
-});
+}
+
+function handleSubmit() {
+	alert("OpenAI call here")
+
+}
+
+function GeneralCharacter() {
+
+	const [toggle, setToggle] = useState(false)
+	const [userInput, setUserInput] = useState("")
+	const { data, loading, error } = useQuery(query);
+
+	const [allAbilityScores, setAllAbilityScores] = useState([])
+	const [allSkills, setAllSkills] = useState([]);
+	const [allLanguages, setAllLanguages] = useState([])
+	const [allAlignments, setAllAlignments] = useState([]);
+	const [allBackgrounds, setAllBackgrounds] = useState([]);
+	const [allClasses, setAllClasses] = useState([]);
+	const [allRaces, setAllRaces] = useState([]);
+	const [allSpells, setAllSpells] = useState([])
+
+	const [profChoices, setProfChoices] = useState({
+		oneprofchoice: false,
+		twoprofchoice: false,
+		threeprofchoice: false
+	})
+
+	var testClass = [];
+	var testClassProf = 0;
+
+
+
+	//if (!loading || error) { //console.log(data); 
+	//	TestStore(data)
+	//}
+	//console.log("-- LOADING: ", loading)
+    if (!loading || error) {
+
+    }
+	useEffect(() => {
+		if (loading === false) {
+			console.log("--- LOADED DATA: ", data)
+			//console.log("classes", allClasses)
+			//console.log("races", allRaces)
+			//console.log("alignments", allAlignments)
+			console.log(Object.entries(data.abilityScores));
+			setAllAbilityScores(Object.entries(data.abilityScores));
+			setAllSkills(Object.entries(data.skills));
+			setAllLanguages(Object.entries(data.languages));
+			setAllAlignments(Object.entries(data.alignments));
+			setAllBackgrounds(Object.entries(data.backgrounds));
+			setAllClasses(Object.entries(data.classes));
+			setAllRaces(Object.entries(data.races));
+			setAllSpells(Object.entries(data.spells));
+		}
+		if (toggle === true) {
+			testClass = allClasses[2]
+			console.log(testClass)
+
+        }
+	}, [loading, error, toggle])
+
+
+
+
+	return (
+		<Layout Title="Let's Get Started...">        
+			<FormDisplay>
+				<form onSubmit={(e) => {
+					if (userInput === '') {
+						e.preventDefault()
+						alert("You must enter a description of your character before proceeding.")
+					}
+					else {
+					e.preventDefault();
+					handleSubmit();
+					setToggle(true)
+					}
+				}}>
+					<input value={userInput} placeholder="Enter a description of your character" onChange={(e) => setUserInput(e.target.value) }/>
+					<button>Submit</button>
+				</form>
+
+				{toggle ?	// eventually will have to include the loading variable of the OpenAI api call
+					<>
+						<h1>Results: </h1>
+						<FlexRow>
+							<FlexColumn>
+								<Dropdown data={allRaces} />
+								<Dropdown data={allClasses} />
+								<SkillDropdown classes={allClasses} />
+							</FlexColumn>
+							<FlexColumn>
+								<AbilityScoreDisplay data={allAbilityScores}/>
+							</FlexColumn>
+						</FlexRow>
+					</>
+					: <></>
+				}
+			</FormDisplay>
+			{toggle ?
+				<ButtonDisplay>
+						<Link href="/backstory">
+							<Action>Continue</Action>
+						</Link>
+					
+				</ButtonDisplay>
+				:
+				<></>
+			}
+		</Layout>
+	)
+}
 
 export default function Home() {
+    const client = new ApolloClient({
+        uri: "https://www.dnd5eapi.co/graphql", 
+        cache: new InMemoryCache()
+    });
     return (
-        <ApolloProvider client={client}>
-            <Layout Title="Let's Get Started...">
-                <GeneralCharacter />
-            </Layout>
-        </ApolloProvider>
+        <ApolloProvider2 client={client}>
+            <ApolloProvider client={client}>
+                <GeneralCharacter/>
+            </ApolloProvider>
+        </ApolloProvider2>
     )
 }
