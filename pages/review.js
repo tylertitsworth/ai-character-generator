c2f9def94ca1322eb897ae080f92e26a17dda325
titswort <titswort@oregonstate.edu>
2022-03-10 15:39:18 -0800

    add state-based method

--- pages/review.js
+++ pages/review.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
-import { PDFViewer } from '@react-pdf/renderer';
-import { PDFDownloadLink } from '@react-pdf/renderer'
+import { Document, Page, usePDF } from '@react-pdf/renderer';
 import Layout from '../components/Layout';
 
 import { Action, ButtonDisplay } from '../styles/globals'
 
 function TestDocument() {
+    console.log(`Building slug: ${slug}`)
     return (
         <Document>
             <Page size="letter">
-                <View>
-                    <Text>Test Section</Text>
-                </View>
+                <div>test 1</div>
             </Page>
             <Page>
-                <Text>Section 2</Text>
+                <div>test 2</div>
             </Page>
         </Document>
     )
 }
 // https://github.com/diegomura/react-pdf
 export default function Review() {
+    const [ PDF, setPDF ] = usePDF({ document: TestDocument })
+    if (PDF.loading) return <div>Loading</div>;
+    if (PDF.error) return <div>Error in PDF Creation</div>;
     return (
         <Layout Title="Review Information">
-            <PDFViewer>
-                <TestDocument/>
-            </PDFViewer>
+        
             <ButtonDisplay>
-                <PDFDownloadLink document={<TestDocument/>} fileName={"AI_Generated_Character_Sheet.pdf"}>
-                    <Action>Download PDF</Action>
-                </PDFDownloadLink>
+                    <Action href={PDF.url} download="AI_Generated_Character_Sheet.pdf" >Download PDF</Action>
                 <Link href="/backstory">
                     <Action>Go Back</Action>
                 </Link>
