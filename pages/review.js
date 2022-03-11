aeaad04780347215f32cfd7fe2c1567286bbc499
Tyler Titsworth <titswort@oregonstate.edu>
2022-03-10 21:43:31 -0800

    Revert "add renderer method to replace react-to-pdf" (#15)

--- pages/review.js
+++ pages/review.js
@@ -1,45 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Link from 'next/link';
-import { Document, Page, Text } from '@react-pdf/renderer';
-import { PDFViewer } from '@react-pdf/renderer';
-import { PDFDownloadLink } from '@react-pdf/renderer'
+import Pdf from 'react-to-pdf'
+
 import Layout from '../components/Layout';
+
 import { Action, ButtonDisplay } from '../styles/globals'
 
-function TestDocument() {
-    // You need to wrap things in the <Text> tag
-    return (
-        <Document>
-            <Page size="letter">
-                <Text>text 1</Text>
-            </Page>
-            <Page>
-                <Text>test 2</Text>
-            </Page>
-        </Document>
-    )
-}
-// https://github.com/diegomura/react-pdf
+const ref = React.createRef();
+
+
+
 export default function Review() {
-    const [ isClient, setIsClient ] = useState(false);
-    useEffect(() => { setIsClient(true); }, [])
     return (
         <Layout Title="Review Information">
-        {isClient && (
-            <>
-                <PDFViewer>
-                    <TestDocument/>
-                </PDFViewer>
-                <ButtonDisplay>
-                    <PDFDownloadLink document={<TestDocument/>} fileName={"AI_Generated_Character_Sheet.pdf"}>
-                        <Action>Download PDF</Action>
-                    </PDFDownloadLink>
-                    <Link href="/backstory">
-                        <Action>Go Back</Action>
-                    </Link>
-                </ButtonDisplay>
-            </>
-        )}
+
+            <div ref={ref}> PDF Container </div>
+            <ButtonDisplay>
+                <Pdf targetRef={ref} filename="AI-Generated-5e-Character.pdf">
+                    {({ toPdf }) => <Action onClick={toPdf}>Download PDF</Action>}
+                </Pdf>
+                <Link href="/backstory">
+                    <Action>Go Back</Action>
+                </Link>
+            </ButtonDisplay>
+
         </Layout>
     )
 }
