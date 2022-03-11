42f5adc2bc534b0605040a0ece1104317e53feff
titswort <titswort@oregonstate.edu>
2022-03-10 17:38:49 -0800

    yarn build successfully + implement a hook to avoid SSR

--- pages/review.js
+++ pages/review.js
@@ -1,42 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { Document, Page, Text } from '@react-pdf/renderer';
 import { PDFViewer } from '@react-pdf/renderer';
 import { PDFDownloadLink } from '@react-pdf/renderer'
 import Layout from '../components/Layout';
-
 import { Action, ButtonDisplay } from '../styles/globals'
 
 function TestDocument() {
+    // You need to wrap things in the <Text> tag
     return (
         <Document>
             <Page size="letter">
-                <View>
-                    <Text>Test Section</Text>
-                </View>
+                <Text>text 1</Text>
             </Page>
             <Page>
-                <Text>Section 2</Text>
+                <Text>test 2</Text>
             </Page>
         </Document>
     )
 }
 // https://github.com/diegomura/react-pdf
 export default function Review() {
+    const [ isClient, setIsClient ] = useState(false);
+    useEffect(() => { setIsClient(true); }, [])
     return (
         <Layout Title="Review Information">
-            <PDFViewer>
-                <TestDocument/>
-            </PDFViewer>
-            <ButtonDisplay>
-                <PDFDownloadLink document={<TestDocument/>} fileName={"AI_Generated_Character_Sheet.pdf"}>
-                    <Action>Download PDF</Action>
-                </PDFDownloadLink>
-                <Link href="/backstory">
-                    <Action>Go Back</Action>
-                </Link>
-            </ButtonDisplay>
-
+        {isClient && (
+            <>
+                <PDFViewer>
+                    <TestDocument/>
+                </PDFViewer>
+                <ButtonDisplay>
+                    <PDFDownloadLink document={<TestDocument/>} fileName={"AI_Generated_Character_Sheet.pdf"}>
+                        <Action>Download PDF</Action>
+                    </PDFDownloadLink>
+                    <Link href="/backstory">
+                        <Action>Go Back</Action>
+                    </Link>
+                </ButtonDisplay>
+            </>
+        )}
         </Layout>
     )
 }
