90b26b58f75146079644d59645850b9f1e0b23a7
titswort <titswort@oregonstate.edu>
2022-03-10 17:23:37 -0800

    Revert "add state-based method"
    This reverts commit c2f9def94ca1322eb897ae080f92e26a17dda325.

--- pages/review.js
+++ pages/review.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import Link from 'next/link';
-import { Document, Page, usePDF } from '@react-pdf/renderer';
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+import { PDFViewer } from '@react-pdf/renderer';
+import { PDFDownloadLink } from '@react-pdf/renderer'
 import Layout from '../components/Layout';
 
 import { Action, ButtonDisplay } from '../styles/globals'
 
 function TestDocument() {
-    console.log(`Building slug: ${slug}`)
     return (
         <Document>
             <Page size="letter">
-                <div>test 1</div>
+                <View>
+                    <Text>Test Section</Text>
+                </View>
             </Page>
             <Page>
-                <div>test 2</div>
+                <Text>Section 2</Text>
             </Page>
         </Document>
     )
 }
 // https://github.com/diegomura/react-pdf
 export default function Review() {
-    const [ PDF, setPDF ] = usePDF({ document: TestDocument })
-    if (PDF.loading) return <div>Loading</div>;
-    if (PDF.error) return <div>Error in PDF Creation</div>;
     return (
         <Layout Title="Review Information">
-        
+            <PDFViewer>
+                <TestDocument/>
+            </PDFViewer>
             <ButtonDisplay>
-                    <Action href={PDF.url} download="AI_Generated_Character_Sheet.pdf" >Download PDF</Action>
+                <PDFDownloadLink document={<TestDocument/>} fileName={"AI_Generated_Character_Sheet.pdf"}>
+                    <Action>Download PDF</Action>
+                </PDFDownloadLink>
                 <Link href="/backstory">
                     <Action>Go Back</Action>
                 </Link>
